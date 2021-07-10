200376945cf89f31ac259e2c33e75b3275d25aeb
dhumill <dhumildholiya97@gmail.com>
2021-07-10 13:25:23 +0530

    imported character animation, tilemaps sprite... Implemented Player Movement.

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,64 @@
+using System.Net.Http.Headers;
+using UnityEngine;
+
+namespace LittleSimTest
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    [RequireComponent(typeof(Animator))]
+    public abstract class CharacterController : MonoBehaviour
+    {
+        [SerializeField] private float speed;
+
+        private Rigidbody2D _rb;
+        private Animator _myAnimator;
+
+        protected Vector2 Direction;
+        private bool IsMoving => Direction.x != 0 || Direction.y != 0;
+
+        protected virtual void Awake()
+        {
+            _rb = GetComponent<Rigidbody2D>();
+            _myAnimator = GetComponent<Animator>();
+        }
+
+        protected virtual void Update()
+        {
+            HandleAnimationLayers();
+        }
+
+        private void FixedUpdate()
+        {
+            Move();
+        }
+
+        private void Move()
+        {
+            _rb.velocity = Direction.normalized * speed;
+        }
+
+        private void HandleAnimationLayers()
+        {
+            if (IsMoving)
+            {
+                ActivateLayer("WalkLayer");
+                _myAnimator.SetFloat("x", Direction.x);
+                _myAnimator.SetFloat("y", Direction.y);
+            }
+            else
+            {
+                ActivateLayer("IdleLayer"); 
+            }
+        }
+
+
+        private void ActivateLayer(string layerName)
+        {
+            for (int i = 0; i < _myAnimator.layerCount; i++)
+            {
+                _myAnimator.SetLayerWeight(i, 0);
+            }
+
+            _myAnimator.SetLayerWeight(_myAnimator.GetLayerIndex(layerName), 1);
+        }
+    }
+}
